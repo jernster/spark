9a7e11516b120e9d31d9f6086036afe4a6d428fe
Jon Ernster <jon.ernster@gmail.com>
2016-09-15 14:54:14 -0600

    Add wrapper scripts to start pyspark and spark shell in sandbox environment.

---
+++ bin/pyspark.sh
@@ -0,0 +1,4 @@
+#~ Wrapper to start pyspark in sandbox
+#~ Jon Ernster - 09/15/2016
+
+pyspark --master yarn-client
---
+++ bin/spark-shell.sh
@@ -0,0 +1,4 @@
+#~ Wrapper to start spark-shell in sandbox
+#~ Jon Ernster - 09/15/2016
+
+spark-shell --master local[2]
